2ff696d913822dfc6346f478614be2431b25e4a4
kmiu2 <kennymiu@rogers.com>
2022-10-12 21:16:11 -0400

    feat: it actually runs

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,22 @@
-import React from 'react'
-import './App.css'
-import logo from './assets/ness-logo.png'
+import { useRoutes } from 'react-router-dom'
+import router from './router'
+
+import AdapterDateFns from '@mui/lab/AdapterDateFns'
+import LocalizationProvider from '@mui/lab/LocalizationProvider'
+
+import { CssBaseline } from '@mui/material'
+import ThemeProvider from './theme/ThemeProvider'
 
 function App() {
+  const content = useRoutes(router)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>NESS site coming soon...</p>
-        <a
-          className="App-link"
-          href="https://discord.gg/rZzAmybEUP"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Join the NESS Discord
-        </a>
-        <p>Useful Links</p>
-        <a
-          className="App-link"
-          href="/courses.png"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Click here for a course map
-        </a>
-        <a
-          className="App-link"
-          href="https://ver7ici.github.io/UWCourseMap/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Click here for a prerequisites checker
-        </a>
-        <p>Contact Us:</p>
-        <a
-          className="App-link"
-          href="mailto:ness@uwaterloo.ca"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          ness@uwaterloo.ca
-        </a>
-      </header>
-    </div>
+    <ThemeProvider>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <CssBaseline />
+        {content}
+      </LocalizationProvider>
+    </ThemeProvider>
   )
 }
-
 export default App
