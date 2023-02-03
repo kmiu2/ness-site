dc1cda29b99b4a7a1eb71ad99b8054080c00478b
kmiu2 <kennymiu@rogers.com>
2023-02-03 16:07:01 -0500

    fix: build works

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom'
 import { HelmetProvider } from 'react-helmet-async'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 import App from './App'
 import { SidebarProvider } from './contexts/SidebarContext'
@@ -9,9 +9,9 @@ import * as serviceWorker from './serviceWorker'
 ReactDOM.render(
   <HelmetProvider>
     <SidebarProvider>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </SidebarProvider>
   </HelmetProvider>,
   document.getElementById('root')
