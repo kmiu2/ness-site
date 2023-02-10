aa3c4b7550014fc6438ef324052d545d2123acbb
kmiu2 <kennymiu@rogers.com>
2023-02-09 21:53:44 -0500

    fix: use hash router ew

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom'
 import { HelmetProvider } from 'react-helmet-async'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 import { SidebarProvider } from './contexts/SidebarContext'
 import * as serviceWorker from './serviceWorker'
@@ -8,9 +8,9 @@ import * as serviceWorker from './serviceWorker'
 ReactDOM.render(
   <HelmetProvider>
     <SidebarProvider>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </SidebarProvider>
   </HelmetProvider>,
   document.getElementById('root')
